965ec82cad44ae5d7c48ab16cbebc19aeb2b6a49
Samir <65616743+Chapali9a@users.noreply.github.com>
2022-02-05 17:09:21 +0100

    initial commit

--- Domain/Models/Reservation.cs
+++ Domain/Models/Reservation.cs
@@ -8,9 +8,9 @@ namespace Domain.Models
     public class Reservation
     {
         public int Id { get; set; }
-        public Room Room { get; set; }
+        public Room? Room { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string Customer { get; set; }
+        public string Customer { get; set; } = string.Empty;
     }
 }
